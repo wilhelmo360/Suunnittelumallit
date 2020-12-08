677dd8b972dc2269d7c61d20a1099a1b3792ce88
wilhelmo360 <32298091+wilhelmo360@users.noreply.github.com>
2020-12-08 20:37:10 +0200

    game

---
+++ Template Method/src/Chess.java
@@ -0,0 +1,26 @@
+class Chess extends Game {
+
+    /* Implementation of necessary concrete methods */
+
+    void initializeGame() {
+        // ...
+    }
+
+    void makePlay(int player) {
+        // ...
+    }
+
+    boolean endOfGame() {
+        // ...
+        return true;
+    }
+
+    void printWinner() {
+        // ...
+    }
+
+    /* Specific declarations for the chess game. */
+
+    // ...
+
+}
---
+++ Template Method/src/Monopoly.java
@@ -0,0 +1,27 @@
+class Monopoly extends Game {
+
+    /* Implementation of necessary concrete methods */
+
+    void initializeGame() {
+        // ...
+    }
+
+    void makePlay(int player) {
+        // ...
+    }
+
+    boolean endOfGame() {
+        // ...
+        return true;
+    }
+
+    void printWinner() {
+        // ...
+    }
+
+    /* Specific declarations for the Monopoly game. */
+
+    // ...
+
+}
+
